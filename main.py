e8c75fb7caa98c092a480ad09be44cd3d0591b33
iiam33 <hongyoong.yeoh@gmail.com>
2023-03-14 13:27:52 +0800

    removed not relevant code

--- main.py
+++ main.py
@@ -2,25 +2,6 @@ from number import Number
 from graphviz import Graph
 from pathlib import Path
 
-# a = Number(data=2.0, label='a')
-# b = Number(data=-3.0, label='b')
-# c = Number(data=10.0, label='c')
-# d = a * b
-# d.label = 'd'
-# e = d + c
-# e.label = 'e'
-# f = Number(data=-2.0, label='f')
-# L = e * f
-# L.label = 'L'
-
-# print('a', a)
-# print('b', b)
-# print('c', c)
-# print('d', d)
-# print('e', e)
-# print('f', f)
-# print('L', L)
-
 pwd = Path.cwd()
 dir = Path.joinpath(pwd, 'graph', 'graph_o2.gv')
 
@@ -77,56 +58,5 @@ o = n.tanh()
 o.label = 'o'
 
 o.backward()
-
-# o.grad = 1.0
-
-# topo = []
-# visited = set()
-
-# def build_topo(root):
-#     if root not in visited:
-#         visited.add(root)
-
-#         for node in root._prev:
-#             build_topo(node)
-        
-#         topo.append(root)
-
-# build_topo(o)
-
-# for node in reversed(topo):
-#     node._backward()
-
-# o._backward()
-# n._backward()
-# x1w1x2w2._backward()
-# x2w2._backward()
-# x1w1._backward()
-
-# # do/dn = 1 - tanh(n)**2
-# # 1 - o.data**2
-# n.grad = 0.5
-
-# # do / dx1w1x2w2
-# x1w1x2w2.grad = 1 * 0.5
-# b.grad = 1 * 0.5
-
-# # do / dx2w2
-# x2w2.grad = 1 * 0.5
-
-# # do / dx1w1
-# x1w1.grad = 1 * 0.5
-
-# # do / dx1
-# x1.grad = w1.data * x1w1.grad
-
-# # do / dw1
-# w1.grad = x1.data * x1w1.grad
-
-# # do / dx2
-# x2.grad = w2.data * x2w2.grad
-
-# # do / dw2
-# w2.grad = x2.data * x2w2.grad
 
 CustomGraph.draw(o)
